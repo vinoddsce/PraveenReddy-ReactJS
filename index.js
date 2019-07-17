29f1abc981c20bbac9bed9fc5bb632b2efaf579d
vinoddsce <vinod.dsce@gmail.com>
2019-07-17 07:47:29 +0530

    XML Http Request

--- index.js
+++ index.js
@@ -1,49 +1,16 @@
-var yearOfBirth = 2000;
+function loadDoc() {
 
-// var vinod = {
-//     firstName: "Vinod",
-//     lastName: "Kumar",
-//     job: "Trainer",
-//     yearOfBirth: 1990,
-//     calculateAge: function (currentYear) {
-//         var innerFunction = (year) => {
-//             return year - this.yearOfBirth;
-//         }
-//         return innerFunction(currentYear);
-//     }
-// }
+    var http = new XMLHttpRequest();
 
-var Person = function (firstName, lastName, yearOfBirth, job) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-}
-
-Person.prototype.display = function () {
-    console.log(this.firstName, this.lastName, this.job);
-}
-
-Person.prototype.calculateAge = function (currentYear) {
-    var innerFunction = (year) => {
-        return year - this.yearOfBirth;
+    http.onreadystatechange = function () {
+        if (this.status === 200) {
+            let result = JSON.stringify(this.responseText);
+            // result = JSON.parse(this.responseText);
+            document.getElementById("data").innerHTML = result;
+        }
     }
-    return innerFunction(currentYear);
+
+    http.open("GET", "https://jsonplaceholder.typicode.com/todos");
+    http.send()
+
 }
-
-var vinod = new Person("Vinod", "Kumar", 1990, 'Teacher');
-console.log(vinod.calculateAge(2019));
-vinod.display();
-var praveen = new Person("Praveen", "Reddy", 1995, 'Designer');
-console.log(praveen.calculateAge(2019));
-praveen.display();
-
-
-
-
-
-
-setTimeout(() => {
-    console.log(vinod.calculateAge(2019));
-}, 5000);
-
